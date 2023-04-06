561a42016a8c7f59f598b122665ebac2fd6a5c4b
baeian <ddoory1103@gmail.com>
2023-04-06 12:23:19 +0900

    랜덤 밈 이미지 렌더링

--- src/MemeGenerator/Components/Meme.js
+++ src/MemeGenerator/Components/Meme.js
@@ -5,29 +5,35 @@ const Meme = () => {
 	// usestate는 렌더링에 적용하고 싶을 때 쓰는 변수 같은 애
 	// destructureing해서 많이 씀
 	// setter 함수로 값을 변경해야함
-	const [randomImg,setRandomImg] = useState()
+	const [memeImg, setMemeImg] = useState(memesData.data.memes[10].url)
 	const getMemeImage = () => {
 		const memesArray = memesData.data.memes
-		const randomNumber = Math.floor(Math.random())
-		let url = memesArray[randomNumber].url
-		console.log(url)
+		const randomNumber = Math.floor(Math.random() * memesArray.length)
+		setMemeImg(memesArray[randomNumber].url)
+		console.log(memeImg)
 	}
 	return (
-		<div className="form">
-			<input
-				type="text"
-				className="form--input"
-				placeholder="write your meme!"
-			/>
-			<input
-				type="text"
-				className="form--input"
-				placeholder="write your meme!"
-			/>
-			<button type="button" onClick={getMemeImage} className="form--button">
-				Get a new meme image
-			</button>
-		</div>
+		<>
+			<div className="form">
+				<input
+					type="text"
+					className="form--input"
+					placeholder="write your meme!"
+				/>
+				<input
+					type="text"
+					className="form--input"
+					placeholder="write your meme!"
+				/>
+				<button
+					type="button"
+					onClick={getMemeImage}
+					className="form--button">
+					Get a new meme image
+				</button>
+			</div>
+			<img src={memeImg} className="meme--image" alt="meme" />
+		</>
 	)
 }
 export default Meme
