d9a7f53d258f9b52ac8baf624cbdfef7bdcb5cde
baeian <ddoory1103@gmail.com>
2023-04-11 17:58:16 +0900

    Travel Journal init

---
+++ src/TravelJournal/TravelJournalApp.js
@@ -0,0 +1,12 @@
+import { Journal } from "./components/Journal"
+import "./style/style.css"
+
+import React from 'react'
+
+export const TravelJournalApp = () => {
+  return (
+	 <>
+	 	<Journal/>
+	 </>
+  )
+}
---
+++ src/TravelJournal/components/JournalList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const JournalList = () => {
+  return (
+	 <div>JournalList</div>
+  )
+}
